f8011d94f221009a283dc0a5c52541ccc72c9529
Alexey Shponarsky <as@jelastic.com>
2020-09-23 15:16:30 +0300

    -- add status servlet

---
+++ src/main/java/com/jelastic/GetStatus.java
@@ -0,0 +1,31 @@
+package com.jelastic;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GetStatus extends HttpServlet {
+
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        out.println("{\"result\": 0}");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       processRequest(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
+}
